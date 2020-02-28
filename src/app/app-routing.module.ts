6104ff6d8c78a9e879fa1d6f83fc98abe2f50c72
William Sedlacek <wsedlacekc@gmail.com>
2020-02-27 21:25:53 -0600

    Lazy Routing
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./lazy/login/login.module').then((m) => m.LoginModule)
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    QuicklinkModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: QuicklinkStrategy })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
