87f418b31e46acb7be7f59085a5d9e6b8b3cd908
William Sedlacek <wsedlacekc@gmail.com>
2020-02-27 20:15:01 -0600

    Remove Placeholder
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public title = 'mud';
-
   @Override()
   public ngOnInit() {}
 }
