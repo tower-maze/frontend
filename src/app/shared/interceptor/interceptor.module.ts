ee0e281a78fdd9ac05d24b09ee9e9d3c57e876e0
William Sedlacek <wsedlacekc@gmail.com>
2020-03-02 20:24:02 -0600

    Cleanup Interceptor Module
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/shared/interceptor/interceptor.module.ts
+++ src/app/shared/interceptor/interceptor.module.ts
@@ -6,12 +6,8 @@ import { ServicesModule } from '../services/services.module';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-];
-
 @NgModule({
   imports: [CommonModule, ServicesModule],
-  providers: [httpInterceptorProviders]
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
 })
 export class InterceptorModule {}
