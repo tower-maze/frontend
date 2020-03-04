306c53e742607c1376f2ec1c1a26d29b3e4f254d
William Sedlacek <wsedlacekc@gmail.com>
2020-03-04 00:44:04 -0600

    More secure interceptor
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/shared/services/services.module.ts
+++ src/app/shared/services/services.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CookieService } from 'ngx-cookie-service';
-
 import { AuthService } from './auth/auth.service';
 
 @NgModule({
   imports: [CommonModule],
-  providers: [AuthService, CookieService]
+  providers: [AuthService]
 })
 export class ServicesModule {}
