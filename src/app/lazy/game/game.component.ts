28cb4c522987c4b6b71ac96aa4bef148ec3633c5
ericbanker12 <ericbanker12@gmail.com>
2020-03-05 11:41:03 -0600

    add arrow key player movement

--- src/app/lazy/game/game.component.ts
+++ src/app/lazy/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Dispatch } from '@ngxs-labs/dispatch-decorator';
+import { Subscription, fromEvent } from 'rxjs';
 
 import { Direction } from '../../models';
 import { Logout } from '../../shared/store/auth/auth.actions';
@@ -9,14 +10,40 @@ import { MovePlayer } from 'src/app/shared/store/game/game.actions';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
+  private movementSubscription: Subscription;
+  
   constructor() {}
 
   @Dispatch()
   public logout = () => new Logout();
 
   @Override()
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.movementSubscription = fromEvent<KeyboardEvent>(document, 'keydown').subscribe((event) => {
+      switch (event.key) {
+        case 'ArrowUp':
+          this.movePlayer('n');
+          break;
+        case 'ArrowDown':
+          this.movePlayer('s');
+          break;
+        case 'ArrowLeft':
+          this.movePlayer('w');
+          break;
+        case 'ArrowRight':
+          this.movePlayer('e');
+          break;
+        default:
+          // ignore other keys
+      }
+    })
+  }
+
+  @Override()
+  public ngOnDestroy() {
+    this.movementSubscription.unsubscribe()
+  }
 
   @Dispatch()
   public movePlayer = (direction: Direction) => new MovePlayer(direction);
