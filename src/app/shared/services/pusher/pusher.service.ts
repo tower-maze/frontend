453d0b175efd188aa7cff90438f50ebec0c17e6f
William Sedlacek <wsedlacekc@gmail.com>
2020-03-05 16:36:58 -0600

    Env Variables
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/shared/services/pusher/pusher.service.ts
+++ src/app/shared/services/pusher/pusher.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import Pusher from 'pusher-js';
+
 import { IOtherModel } from '../../../models';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PusherService {
   private movement$: Subject<IOtherModel> = new Subject<IOtherModel>();
-  private pusherClient = new Pusher('4e141243abf4aa14dd30', { cluster: 'us3' });
+  private pusherClient = new Pusher(environment.pusherKey, { cluster: environment.pusherCluster });
 
   constructor() {
-    const channel = this.pusherClient.subscribe('Tower-Maze');
+    const channel = this.pusherClient.subscribe(environment.pusherChannel);
     channel.bind('movement', (movement: IOtherModel) => this.movement$.next(movement));
   }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://tower-maze.herokuapp.com'
+  apiURL: 'https://tower-maze.herokuapp.com',
+  pusherKey: '4e141243abf4aa14dd30',
+  pusherCluster: 'us3',
+  pusherChannel: 'Tower-Maze'
 };
